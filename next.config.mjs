a0404642de0013c3593974631e0ee029727882ba
nibhua <hetuprajapat222@gmail.com>
2025-09-02 14:43:29 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -10,13 +10,13 @@ const nextConfig = {
     unoptimized: true,
   },
 
-  // 👇 Add env mapping here
+  // 👇 Make Railway happy
+  output: "standalone",
+
   env: {
     NEXT_PUBLIC_ADOBE_EMBED_API_KEY: process.env.ADOBE_EMBED_API_KEY,
     NEXT_PUBLIC_API_BASE: process.env.NEXT_PUBLIC_API_BASE || "http://127.0.0.1:8000"
-  },
-
-  // Removed proxy rewrites - now calling backend directly
+  }
 }
 
 export default nextConfig
